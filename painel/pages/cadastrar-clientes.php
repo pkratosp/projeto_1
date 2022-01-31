5e4e5512a6ede0cba63a240767bde965134ea94e
pkratosp <p20142004@gmail.com>
2022-01-31 20:55:38 -0300

    Update cadastrar-clientes.php

--- painel/pages/cadastrar-clientes.php
+++ painel/pages/cadastrar-clientes.php
@@ -2,7 +2,24 @@
 
 verificaPermissaoPagina(2); 
 
+if(isset($_POST['acao'])){
 
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $tipo = $_POST['tipo_cliente'];
+    $cpf = $_POST['cpf'];
+    $cnpj = $_POST['cnpj'];
+    $image = $_FILES['image'];
+
+    $infoFinal = $cpf == '' ? $cnpj : $cpf;
+
+        $image = Painel::updateImage($image);
+        $arr = ['nome'=>$nome, 'email'=>$email, 'tipo'=>$tipo, 'cpf_cnpj'=>$infoFinal, 'image'=>$image, 'nome_tabela'=>'tb_admin.cliente'];
+        Painel::IncerirDepoimento($arr);
+        Painel::AtualizarAlerta('true','O cliente foi cadastrado com sucesso.');
+
+
+}
 
 ?>
 
@@ -16,17 +33,17 @@ verificaPermissaoPagina(2);
 
         <div class="alinhe-inputs">
             <label>Nome:</label>
-            <input type="text" name="nome" required>
+            <input type="text" name="nome">
         </div><!--alinhe-inputs-->
 
         <div class="alinhe-inputs">
             <label>Email:</label>
-            <input type="text" name="email" required>
+            <input type="text" name="email">
         </div><!--alinhe-inputs-->
 
         <div class="alinhe-inputs">
             <label>Tipo:</label>
-            <select name="tipo_cliente" required>
+            <select name="tipo_cliente">
                 <option value="fisico">Físico</option>
                 <option value="juridico">Jurídico</option>
             </select>
@@ -34,17 +51,17 @@ verificaPermissaoPagina(2);
 
         <div rel="cpf" class="alinhe-inputs">
             <label>CPF:</label>
-            <input type="text" name="cpf" required>
+            <input type="text" name="cpf">
         </div><!--alinhe-inputs-->
 
         <div style="display:none;" rel="cnpj" class="alinhe-inputs">
             <label>CNPJ:</label>
-            <input type="text" name="cnpj" required>
+            <input type="text" name="cnpj">
         </div><!--alinhe-inputs-->
 
         <div class="alinhe-inputs">
             <label>Imagem:</label>
-            <input type="file" name="image" required>
+            <input type="file" name="image">
         </div><!--alinhe-inputs-->
 
         <div class="alinhe-inputs">
